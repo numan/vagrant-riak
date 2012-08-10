053554f1654c4e5bf58ec302f2a925c3a9231e1a
H.Y. Leung <hy.leung@gmail.com>
2012-08-09 20:28:09 -0700

    Added download of Riak package
    Provisioning a /tmp/downloads directory to store downloaded files.
    Also apt-get'd libssl dependencies.

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,9 +5,34 @@ class development {
 		command => '/usr/bin/apt-get update --fix-missing',
 	}
 
+	package {
+		"curl":
+		ensure => installed
+	}
+
 	group { "puppet":
      	ensure => "present",
    	}
+
+	#ensure directory for downloads
+	file {
+		'/tmp/downloads':
+		ensure => directory,
+		group => 'puppet'
+	}
+
+	#install libssl
+	package {
+		"libssl0.9.8":
+		ensure => installed
+	}
+	#download the Riak binary package
+	exec { 'wget-riak':
+		command => '/usr/bin/wget http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/CURRENT/ubuntu/precise/riak_1.2.0-1_amd64.deb -O /tmp/downloads/riak_1.2.0-1_amd64.deb',
+		creates => "/tmp/downloads/riak_1.2.0-1_amd64.deb"
+		}
+
+
 }
 
 include development
